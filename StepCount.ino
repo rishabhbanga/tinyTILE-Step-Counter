1470afbd84f6b1a1e4fb6eb586862c8aebac4fe1
Rishabh <rishabh.banga@ericsson.com>
2017-03-17 05:11:24 +0530

    Add files via upload

---
+++ StepCount.ino
@@ -0,0 +1,33 @@
+#include "CurieIMU.h"
+
+boolean stepEventsEnabeled = true;   
+int lastStepCount = 0;             
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial) ;  
+  CurieIMU.begin();
+  CurieIMU.setStepDetectionMode(CURIE_IMU_STEP_MODE_SENSITIVE);
+  CurieIMU.setStepCountEnabled(true);
+  if (stepEventsEnabeled) {
+    CurieIMU.attachInterrupt(eventCallback);
+    CurieIMU.interrupts(CURIE_IMU_STEP);
+    Serial.println("IMU initialisation complete, waiting for events...");
+  }
+}
+
+void loop() {}
+
+static void updateStepCount() {
+  int stepCount = CurieIMU.getStepCount();
+  if (stepCount != lastStepCount) {
+    Serial.print("Step count: ");
+    Serial.println(stepCount);
+    lastStepCount = stepCount;
+  }
+}
+
+static void eventCallback(void) {
+  if (CurieIMU.stepsDetected())
+    updateStepCount();
+}
